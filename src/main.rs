7f6ba720af18a7911c55733320e2b079e9679622
ilyJoyce <ztron195@proton.me>
2025-06-25 09:53:59 +0200

    Osen-Webserver-Creation

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use axum::{
+    routing::get,
+    Router,
+};
+use tower_http::services::{ServeDir, ServeFile};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // ServeDir für statische Dateien aus "assets"
+    let static_service = ServeDir::new("..")
+        .not_found_service(ServeFile::new("../index.html"));
+
+    let app = Router::new()
+        .fallback_service(static_service);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 6464));
+    println!("Server gestartet auf http://{}", addr);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:6464").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
